583b6b5ec1036b708e39c311cef165ab687bcec8
Pivarcsi Gergely Máté <gergely.pivarcsi@gmail.com>
2021-11-18 11:51:08 +0100

    "String[] szavak = br.readLine().split(";");". I love learning, unironically.

--- src/hu/petrik/morzeoop/Main.java
+++ src/hu/petrik/morzeoop/Main.java
@@ -4,25 +4,30 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
+    public static HashMap<String, String> morzeAbcReversed;
+    public static HashMap<String, String> morzeAbc;
 
     public static void main(String[] args) {
         BeolvasABC();
+        KiirABC();
     }
 
     public static void BeolvasABC() {
-        HashMap<String, String> morzeAbc = new HashMap<>();
-        HashMap<String, String > morzeAbcReversed = new HashMap<>();
+        morzeAbc = new HashMap<>();
+        morzeAbcReversed = new HashMap<>();
 
         try {
             BufferedReader br = new BufferedReader(new FileReader("morzeabc.txt"));
+            String[] szavak = br.readLine().split(";");
             String sor = br.readLine();
             while (sor != null){
-                br.readLine();
-                String[] st = sor.split("\t");
+
+                String[] st = sor.split("\\t", -1);
                 String betu = st[0];
-                String morzeJel = st[0];
+                String morzeJel = st[1];
                 morzeAbc.put(betu, morzeJel);
                 morzeAbcReversed.put(morzeJel, betu);
                 sor = br.readLine();
@@ -31,6 +36,12 @@ public class Main {
         }
         catch (IOException e){
             e.getMessage();
+        }
+    }
+
+    private static void KiirABC() {
+        for (Map.Entry<String, String> entry : morzeAbc.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
         }
     }
 }
