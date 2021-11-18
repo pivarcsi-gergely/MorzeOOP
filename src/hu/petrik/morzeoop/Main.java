829f432d8a7eacf0895d7c5615a99c64325820fe
Pivarcsi Gergely Máté <gergely.pivarcsi@gmail.com>
2021-11-18 11:36:41 +0100

    - MorzeABC + BeolvasABC

--- src/hu/petrik/morzeoop/Main.java
+++ src/hu/petrik/morzeoop/Main.java
@@ -1,8 +1,36 @@
 package hu.petrik.morzeoop;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        BeolvasABC();
+    }
+
+    public static void BeolvasABC() {
+        HashMap<String, String> morzeAbc = new HashMap<>();
+        HashMap<String, String > morzeAbcReversed = new HashMap<>();
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("morzeabc.txt"));
+            String sor = br.readLine();
+            while (sor != null){
+                br.readLine();
+                String[] st = sor.split("\t");
+                String betu = st[0];
+                String morzeJel = st[0];
+                morzeAbc.put(betu, morzeJel);
+                morzeAbcReversed.put(morzeJel, betu);
+                sor = br.readLine();
+            }
+            br.close();
+        }
+        catch (IOException e){
+            e.getMessage();
+        }
     }
 }
